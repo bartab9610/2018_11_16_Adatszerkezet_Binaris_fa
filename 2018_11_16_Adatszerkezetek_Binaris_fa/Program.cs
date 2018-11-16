c270cdf36ce2a5ce97501042260ff5590252eab7
bartab9610 <barnabas96@freemail.hu>
2018-11-16 14:55:57 +0100

    IList

--- 2018_11_16_Adatszerkezetek_Binaris_fa/Program.cs
+++ 2018_11_16_Adatszerkezetek_Binaris_fa/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,19 @@ namespace _2018_11_16_Adatszerkezetek_Binaris_fa
             public string Teljes_nev { get; private set; }
             public DateTime Szuldatum { get; private set; }
         }
+        static bool Van_e_A_betus(ISet<Szemelyi_Adatok> szemelyek)
+        {
+            foreach (var e in szemelyek)
+            {
+                if (e.Teljes_nev.StartsWith("A"))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
         static void Main(string[] args)
         {
-            // https://github.com/bartab9610/2018_11_16_Adatszerkezet_Binaris_fa.git
             ISet<string> halmaz = new SortedSet<string>();
 
             halmaz.Add("Elem_6");
@@ -52,7 +63,8 @@ namespace _2018_11_16_Adatszerkezetek_Binaris_fa
             Console.WriteLine(magassag);
             Console.WriteLine();
 
-            IDictionary<string, Szemelyi_Adatok> dict2 = new SortedDictionary<string, Szemelyi_Adatok>();
+            IDictionary<string, Szemelyi_Adatok> dict2 = new Dictionary<string, Szemelyi_Adatok>();
+                                                        // new SortedDictionary<string, Szemelyi_Adatok>();
             dict2.Add("Kovacs_bela", new Szemelyi_Adatok("Kovacs Bela", new DateTime(1996, 1, 1)));
             string username = "Kovacs_bela";
             if (dict2.ContainsKey(username))
@@ -60,6 +72,10 @@ namespace _2018_11_16_Adatszerkezetek_Binaris_fa
                 Szemelyi_Adatok adatok = dict2[username];
                 Console.WriteLine(adatok.Teljes_nev);
             }
+
+            // Java: ArrayList
+            // IList<string> lista = new BindingList<string>();
+
             Console.ReadKey();
         }
     }
